d7008af25bf7a5ebf0bb83ecd775f0a116877b2a
AlvisonHunterArnuero <alvison@gmail.com>
2024-05-23 17:28:39 -0600

    Added init proj config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
