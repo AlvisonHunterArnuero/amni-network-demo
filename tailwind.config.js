bb2cd29fe10b9b2ec2bfb51f457506c03b732a3f
AlvisonHunterArnuero <alvison@gmail.com>
2024-05-24 20:21:22 -0600

    Override Attempt to add custom color names to existing hex codes on the components

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        primary: 'var(--primary-color)',
+        secondary: 'var(--secondary-color)',
+        warm: 'var(--warm-color)',
+        muted: 'var(--muted-color)',
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
